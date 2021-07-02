1c17da9bc6e93761b0c78184873c1817d4b721e8
ip-130 <85837397+ip-130@users.noreply.github.com>
2021-07-02 14:00:29 +0300

    init

--- app/src/main/java/com/google/homework_6_msv/MainActivity.java
+++ app/src/main/java/com/google/homework_6_msv/MainActivity.java
@@ -2,104 +2,74 @@ package com.google.homework_6_msv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import androidx.fragment.app.Fragment;
-
-import android.app.DatePickerDialog;
-import android.app.FragmentTransaction;
-import android.app.TimePickerDialog;
-import android.os.Bundle;
-import android.text.format.DateUtils;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.DatePicker;
-import android.widget.TextView;
-import android.widget.TimePicker;
-
-import java.util.Calendar;
-
 public class MainActivity extends AppCompatActivity {
 
-    Fragment fragment1;
-    Fragment fragment2;
-    FragmentTransaction fragmentTransaction;
-    CheckBox chbStack;
-    TextView currentDateTime;
-    Calendar dateAndTime = Calendar.getInstance();
+    public String title;
+    public String text;
+    public String createData;
+    public String changeData;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        currentDateTime = (TextView) findViewById(R.id.currentDateTime);
-        setInitialDateTime();
+    Fragment_1 fragment1;
+    Fragment_2 fragment2;
 
 
-        fragment1 = new fragment1();
-        fragment2 = new fragment2();
 
-        chbStack = (CheckBox) findViewById(R.id.chbStack);
-    }
+//    TextView currentDateTime;
+//    Calendar dateAndTime = Calendar.getInstance();
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        currentDateTime = (TextView) findViewById(R.id.currentDateTime);
+//        setInitialDateTime();
+//    }
+//
+//
+//
+//    public void setDate(View v) {
+//        new DatePickerDialog(MainActivity.this, d,
+//                dateAndTime.get(Calendar.YEAR),
+//                dateAndTime.get(Calendar.MONTH),
+//                dateAndTime.get(Calendar.DAY_OF_MONTH))
+//                .show();
+//    }
+//
+//    // отображаем диалоговое окно для выбора времени
+//    public void setTime(View v) {
+//        new TimePickerDialog(MainActivity.this, t,
+//                dateAndTime.get(Calendar.HOUR_OF_DAY),
+//                dateAndTime.get(Calendar.MINUTE), true)
+//                .show();
+//    }
+//
+//    // установка начальных даты и времени
+//    private void setInitialDateTime() {
+//
+//        currentDateTime.setText(DateUtils.formatDateTime(this,
+//                dateAndTime.getTimeInMillis(),
+//                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR
+//                        | DateUtils.FORMAT_SHOW_TIME));
+//    }
+//
+//    // установка обработчика выбора времени
+//    TimePickerDialog.OnTimeSetListener t = new TimePickerDialog.OnTimeSetListener() {
+//        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+//            dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+//            dateAndTime.set(Calendar.MINUTE, minute);
+//            setInitialDateTime();
+//        }
+//    };
+//
+//    // установка обработчика выбора даты
+//    DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener() {
+//        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+//            dateAndTime.set(Calendar.YEAR, year);
+//            dateAndTime.set(Calendar.MONTH, monthOfYear);
+//            dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+//            setInitialDateTime();
+//        }
+//    };
 
-    public void setDate(View v) {
-        new DatePickerDialog(MainActivity.this, d,
-                dateAndTime.get(Calendar.YEAR),
-                dateAndTime.get(Calendar.MONTH),
-                dateAndTime.get(Calendar.DAY_OF_MONTH))
-                .show();
-    }
-
-    // отображаем диалоговое окно для выбора времени
-    public void setTime(View v) {
-        new TimePickerDialog(MainActivity.this, t,
-                dateAndTime.get(Calendar.HOUR_OF_DAY),
-                dateAndTime.get(Calendar.MINUTE), true)
-                .show();
-    }
-
-    // установка начальных даты и времени
-    private void setInitialDateTime() {
-
-        currentDateTime.setText(DateUtils.formatDateTime(this,
-                dateAndTime.getTimeInMillis(),
-                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR
-                        | DateUtils.FORMAT_SHOW_TIME));
-    }
-
-    // установка обработчика выбора времени
-    TimePickerDialog.OnTimeSetListener t = new TimePickerDialog.OnTimeSetListener() {
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
-            dateAndTime.set(Calendar.MINUTE, minute);
-            setInitialDateTime();
-        }
-    };
-
-    // установка обработчика выбора даты
-    DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener() {
-        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-            dateAndTime.set(Calendar.YEAR, year);
-            dateAndTime.set(Calendar.MONTH, monthOfYear);
-            dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-            setInitialDateTime();
-        }
-    };
-
-    public void onClick(View v) {
-        fragmentTransaction = getFragmentManager().beginTransaction();
-        switch (v.getId()) {
-            case R.id.btnAdd:
-                fragmentTransaction.add(R.id.frgmCont, fragment1);
-                break;
-            case R.id.btnRemove:
-                fragmentTransaction.remove(fragment1);
-                break;
-            case R.id.btnReplace:
-                fragmentTransaction.replace(R.id.frgmCont, fragment2);
-            default:
-                break;
-        }
-        if (chbStack.isChecked()) fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
-    }
 
 }
